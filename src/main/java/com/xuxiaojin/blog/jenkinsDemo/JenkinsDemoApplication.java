a677bdba8fc5a0351e9394cd64ea7d09183460f6
xuxiaojin <1319039722@qq.com>
2020-06-03 19:38:10 +0800

    test jenkins

--- src/main/java/com/xuxiaojin/blog/jenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/xuxiaojin/blog/jenkinsDemo/JenkinsDemoApplication.java
@@ -8,7 +8,7 @@ public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins this is change");
     }
 
 }
